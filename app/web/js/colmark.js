e3b58112b0c41eaf7651a07c59de69b676ae5ff9
Markus Mächler <markus.maechler@bithost.ch>
2016-02-01 14:55:43 +0100

    Adds markdown js

--- app/web/js/colmark.js
+++ app/web/js/colmark.js
@@ -2,6 +2,18 @@
 
 angular.module('colmark', [])
 
-.controller('MainController', function() {
+.controller('MainController', function($scope) {
+	angular.element(document).ready(function () {
 
+
+		function Editor(input, preview) {
+			this.update = function () {
+				preview.innerHTML = markdown.toHTML(input.value);
+			};
+			this.update();
+		}
+		new Editor($("#editor"), $("#preview"));
+
+		console.log("init");
+	});
 });
