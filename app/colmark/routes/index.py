3ae93507d173bf5b707703602799b3618fc702ae
Florian Bruggisser <florian@nexpose.ch>
2016-02-01 15:29:44 +0100

    pep8 fix

--- app/colmark/routes/index.py
+++ app/colmark/routes/index.py
@@ -1,8 +1,10 @@
 from colmark import app
 from flask import render_template
+
 from colmark import config
 
 
 @app.route('/')
 def root():
     return render_template('index.html', resource=config.WEB_STATIC_PATH)
+
